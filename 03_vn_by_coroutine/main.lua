ecd51e7df310d13f3185a3f326e38c1a90d2e296
Jakob Eklund <jordgubben@users.noreply.github.com>
2025-03-16 09:38:45 +0100

    Treat plot as an itterator function
    Plot is a itterator function that as long as there is something to show
    returns the next thing to show, and nil once it's depleated.
    
    This should make it usable in a for each loop (if that's ever needed).
    
    Also, minimal logging infrastructure in place.
    
    Bonus: Eccentric function naming convention came to me in a (day-)dream.

--- 03_vn_by_coroutine/main.lua
+++ 03_vn_by_coroutine/main.lua
@@ -1,4 +1,6 @@
--- [[ Minimal VN interface using coroutines as controllflow. ]]
+-- [[ Minimal VN interface using coroutines as control flow. ]]
+--
+-- Naming convention: field names use lower camel case, with underscore for prefix and suffix categorization.
 
 
 -- Global state (sue me, or don't).
@@ -15,37 +17,55 @@ message = ""
 --
 function funcLocation ( depth )
 	local info = debug.getinfo( (depth or 0) + 2 )
-	return string.format( "%s:%d", info.source, info.linedefined )
+	return string.format( "%s:%d\t%s()", info.source, info.linedefined, info.name or "<anonymous>" )
 end
+
+
+-- Print log message
+function log_internal ( level, msg )
+	print( level, funcLocation( 2 ), msg )
+end
+function log_info( msg ) log_internal( "INFO", msg ) end
+function log_error( msg ) log_internal( "ERROR", msg ) end
+
 
 
 -- Display a text message (without options).
 function showMessage( msg )
-	message = msg
-	coroutine.yield( funcLocation( 1 ) )
+	log_info( msg )
+	coroutine.yield( msg )
 end
 
 
 -- Coroutine used to forward monologue.
-plot = coroutine.create( function ()
-	print( "PLOT:", "Hello, Coroutine!")
+plot = coroutine.wrap( function ()
+	log_info( "Hello, Coroutine!")
 
 	showMessage( "Greetings!" )
 	showMessage( "How do you do?" )
 	showMessage( "That's good to hear!" )
 
-	print( "PLOT:", "That's it!")
+	log_info( "That's it!")
 end)
 
 
 -- Forward plot one step (i.e. handle player key press)
 function forwardPlot ()
-	local ongoing, msg = coroutine.resume( plot )
-	if ongoing then
-		print( "Ongoing:", msg )
+	if  not plot then
+		log_info( "We're done here" )
+		return
+	end
+
+	local ok, result = pcall( plot )
+	if not ok then
+		log_error( result )
+	elseif result then
+		log_info( string.format("Progress: '%s'", result ) )
+		message = result
 	else
-		print( "Concluded:", msg )
+		log_info( "Concluded!")
 		message = ""
+		plot = nil
 	end
 end
 
@@ -90,7 +110,7 @@ end
 
 -- Handle key press input as stream of event.
 function love.keypressed( key, scancode, isrepeat )
-	print ( "Key pressed:", key, scancode, isrepeat )
+	log_info( string.format("Key pressed: %s\t%s\t%s", key, scancode, isrepeat ) )
 
 	if  key == "space" and not isrepeat then
 		forwardPlot()
